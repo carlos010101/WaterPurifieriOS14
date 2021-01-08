87106ed6a913a30f9b999538b2c4f457c57715e9
Carlos Hernandez <chedezv@gmail.com>
2021-01-07 22:03:54 -0600

    add Constants

---
+++ Water Purifier/Water Purifier/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Water Purifier
+//
+//  Created by mac on 07/01/21.
+//
+
+import Foundation
+
+let garrafonCompletoSegue = "garrafonCompletoSegue"
+let medioGarrafonSegue = "medioGarrafonSegue"
+let botellaSegue = "botellaSegue"
